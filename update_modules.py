9210929f5394ff8d4ab48f3053f440fd1caf6eed
Quartermaster (Deploy) <drgn@dragons.moe>
2016-04-11 01:53:03 -0400

    Added module to pull updates from git

---
+++ update_modules.py
@@ -0,0 +1,9 @@
+import sopel.module as module
+import git
+
+@module.commands('gitpull')
+@module.require_admin()
+def pull(bot, trigger):
+	g = git.cmd.Git('/home/quartermaster/.sopel/modules')
+	res = g.pull()
+	bot.say(f)
